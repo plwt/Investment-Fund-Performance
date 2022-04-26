baa193c99469b3ebb176131b920ee264a1613c42
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-04-26 11:10:55 +0100

    Update Investment Fund Performance.py

--- src/Investment Fund Performance.py
+++ src/Investment Fund Performance.py
@@ -1,17 +1,22 @@
-import datetime
 from datetime import date
 
 def getstartvalue():
-
+    
+    # Gets the initial value of an investment
+    
     startvalue = int(input("Enter the starting value of the investment: "))
     return startvalue
 
 def getendvalue():
+    
+    # Gets the end value of an investment
 
     endvalue = int(input("Enter the end value of the investment: "))
     return endvalue
 
 def getstartdate():
+    
+    # Gets the start date of the investment period
     
     date_entry = input('Enter the start date in DD-MM-YYYY format: ')
     year, month, day = map(int, date_entry.split('-'))
@@ -20,12 +25,16 @@ def getstartdate():
 
 def getenddate():
     
+     # Gets the end date of the investment period
+    
     date_entry = input('Enter the end date in DD-MM-YYYY format: ')
     year, month, day = map(int, date_entry.split('-'))
     enddate = datetime.date(day, month, year)
     return enddate
 
-def date_factor():
+def getyearfrac():
+    
+    # Calculates the fraction of the year represented by the number of whole days between the start and end dates
     
     startdate=getstartdate()
     enddate=getenddate()
@@ -35,11 +44,13 @@ def date_factor():
 
 def calc():
     
+    # Calculates the investment performance and provides an output to the user
+    
     startvalue=getstartvalue()
     endvalue=getendvalue()
-    yearf=date_factor()
+    yearfrac=getyearfrac()
     
-    ifp=((endvalue/startvalue)**(1/(yearf))-1)*100
+    ifp=((endvalue/startvalue)**(1/(yearfrac))-1)*100
     
     print(f"Your investment has grown by a compound annual growth rate of {ifp}%")
     
